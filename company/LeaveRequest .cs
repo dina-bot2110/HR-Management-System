035a45ace8caa8d074ef8c52ccdaf047b5d3ce50
dina-bot2110 <dinaiks2004@gmail.com>
2025-09-06 20:26:11 +0300

    your descriptive massage here

--- company/LeaveRequest .cs
+++ company/LeaveRequest .cs
@@ -12,14 +12,14 @@ namespace company
         public int Days;
         public string Reason;
         public string Status; //Pending / Approved / Rejected
-        public LeaveRequest(int emId, int days, string reason, string status)
+        public LeaveRequest(int emId, int days, string reason, string status)//construct
         {
             this.emId = emId;
             Days = days;
             Reason = reason;
             Status = status;
         }
-        public static void ViewLeaveRequests(List<LeaveRequest> leaveRequests)
+        public static void ViewLeaveRequests(List<LeaveRequest> leaveRequests)//عرض قائمة الاجازات
         {
             Console.WriteLine("{0,-3} {1,-10} {2,-6} {3,-25} {4,-10}",
                 "#", "EmpId", "Days", "Reason", "Status");
@@ -35,20 +35,24 @@ namespace company
                     leaveRequests[i].Status);
             }
         }
-        public static void Approval_of_leave(int index, List<LeaveRequest> leaveRequests)
+        public static void Approval_of_leave(int index, List<LeaveRequest> leaveRequests)//قبول الاجازات
         {
+            //تغيير حالة الاجازة من الانتظار للموافقة
             leaveRequests[index].Status = "Approved";
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Leave request is updated successfully.");
             Console.ForegroundColor = ConsoleColor.White;
+            //اضافة العملية في ملف الsysteam
             logger.Log($"Leave request Approved for Employee ID: {leaveRequests[index].emId} → Days: {leaveRequests[index].Days}");
         }
-        public static void Refusal_to_leave(int index, List<LeaveRequest> leaveRequests)
+        public static void Refusal_to_leave(int index, List<LeaveRequest> leaveRequests)//رفض الاجازات
         {
+            //تغير حالة الاجازة للرفض
             leaveRequests[index].Status = "Rejected";
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Leave request is updated successfully.");
             Console.ForegroundColor = ConsoleColor.White;
+            //اضافة العملية في ملف الsysteam
             logger.Log($"Leave request Rejected for Employee ID: {leaveRequests[index].emId} → Days: {leaveRequests[index].Days}, Reason: {leaveRequests[index].Reason}");
         }  
          
